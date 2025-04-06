f653859fbd4d84a6a2ef27ea9d7a8458319efc45
B00M3RSIX <michael.krauss.sw@gmail.com>
2025-04-06 10:13:17 +0200

    Basic Setup & blink sketch

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,39 @@
 #include <Arduino.h>
+// Pin definitions
+const int ledPin = 13;  // Teensy 4.0 has an onboard LED connected to pin 13
 
-// put function declarations here:
-int myFunction(int, int);
+// Variables
+int ledState = LOW;           // LED state
+unsigned long previousMillis = 0;  // Store the last time LED was updated
+const long interval = 1000;        // Blink interval (milliseconds)
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Initialize the LED pin as an output
+  pinMode(ledPin, OUTPUT);
+  
+  // Optional: Start serial for debugging
+  Serial.begin(9600);
+  Serial.println("Teensy 4.0 Blink Sketch Started");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Check if it's time to blink the LED
+  unsigned long currentMillis = millis();
+  
+  if (currentMillis - previousMillis >= interval) {
+    // Save the last time you blinked the LED
+    previousMillis = currentMillis;
+    
+    // Toggle the LED state
+    if (ledState == LOW) {
+      ledState = HIGH;
+      Serial.println("LED ON");
+    } else {
+      ledState = LOW;
+      Serial.println("LED OFF");
+    }
+    
+    // Update the LED
+    digitalWrite(ledPin, ledState);
+  }
 }
